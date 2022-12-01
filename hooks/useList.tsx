edda83e4bfb72216d5a4a6a029c3a66a36105428
Sadiq <sadiqneymaaa@gmail.com>
2022-11-30 21:03:56 -0700

    finally

---
+++ hooks/useList.tsx
@@ -0,0 +1,59 @@
+// import { useEffect, useState } from 'react'
+// import {
+//   onCurrentUserSubscriptionUpdate,
+//   Subscription,
+// } from '@stripe/firestore-stripe-payments'
+// import payments from '../lib/stripe'
+// import { User } from 'firebase/auth'
+
+// function useSubscription(user: User | null) {
+//   const [subscription, setSubscription] = useState<Subscription | null>(null)
+
+//   useEffect(() => {
+//     if (!user) return
+
+//     onCurrentUserSubscriptionUpdate(payments, (snapshot) => {
+//       setSubscription(
+//         snapshot.subscriptions.filter(
+//           (subscription) =>
+//             subscription.status === 'active' ||
+//             subscription.status === 'trialing'
+//         )[0]
+//       )
+//     })
+//   }, [user])
+
+//   return subscription
+// }
+
+// export default useSubscription
+
+
+import { collection, DocumentData, onSnapshot } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { db } from '../firebase'
+import { Movie } from '../typings'
+
+function useList(uid: string | undefined) {
+  const [list, setList] = useState<DocumentData[] | Movie[]>([])
+
+  useEffect(() => {
+    if (!uid) return
+
+    return onSnapshot(
+      collection(db, 'customers', uid, 'myList'),
+      (snapshot) => {
+        setList(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+        )
+      }
+    )
+  }, [db, uid])
+
+  return list
+}
+
+export default useList
